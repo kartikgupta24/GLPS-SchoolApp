be8dddf3db54adedd816326458bacdeee85babe3
kartikgupta24 <kartikgupta1gb@gmail.com>
2025-09-20 23:57:20 +0530

    commit

--- GLPSFeesApp.py
+++ GLPSFeesApp.py
@@ -21,7 +21,7 @@ def get_db_connection():
             database=os.getenv('DB_NAME'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
-            port=os.getenv('DB_PORT')
+            port=os.getenv('DB_PORT'),
             sslmode="require"
         )
         return conn
